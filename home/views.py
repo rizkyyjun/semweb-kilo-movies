23198c27976bb287eb98f334f08c485fb50ded8e
Rahadian Panji <rahadianpanjipanji@gmail.com>
2022-12-20 03:18:53 +0700

    implement Movie Detail

--- home/views.py
+++ home/views.py
@@ -35,9 +35,38 @@ def movie_search(request):
     return render(request, 'movie_search.html', ctx)
 
 
-def movie_detail(request):
-    data = {'title': 'Wednesday', 'Actors': [{'name': 'Jenna Ortega'}]}
-    return render(request, 'movie_detail.html', data)
+def movie_detail(request, movie_id):
+    # TODO: get detail from backend function
+    ctx = {
+        'id': 2, 
+        'title': 'Wednesday', 
+        'description': 'Hari Rabu',
+        'director': 'Christopher Nolan', 
+        'rating': 4.8, 
+        'genre': 'Thriller',
+        'actors': [{'id': 1, 'name': 'Sule'}, {'id': 2, 'name': 'Andre'}],
+        'year': 2021,
+        'runtime': 201,
+        'votes': 12,
+        'rank': 2,
+        'revenue': 128
+    }
+    if movie_id == 1:
+        ctx = {
+            'id': 1, 
+            'title': 'wot u doin step brother', 
+            'description': 'Stuck in washing machine',
+            'director': 'Pronbuh', 
+            'rating': 9.9, 
+            'genre': 'Family',
+            'actors': [{'id': 3, 'name': 'Sis'}, {'id': 4, 'name': 'Bro'}],
+            'year': 2077,
+            'runtime': 15,
+            'votes': 51534,
+            'rank': 1,
+            'revenue': 696969
+        }
+    return render(request, 'movie_detail.html', ctx)
 
 
 def actor_detail(request):
