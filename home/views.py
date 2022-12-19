b81ef8b77ec94f6bf74d47b0fcc650dbc47241b4
gbhisma <bhismagregorius@gmail.com>
2022-12-19 19:55:38 +0700

    init project: add home url

--- home/views.py
+++ home/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 from SPARQLWrapper import SPARQLWrapper, JSON
 import rdflib
+
+def index(request):
+    data = {'genres': ['action', 'romance', 'comedy']} # stubs, TODO: query from ttl to get list of genre
+    return render(request, 'home.html', data)
