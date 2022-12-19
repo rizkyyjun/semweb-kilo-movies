dbe1b075b1d1efca8e52a71b210a28ac02494a8f
Rahadian Panji <rahadianpanjipanji@gmail.com>
2022-12-20 02:35:15 +0700

    fix homepage and implement movie-search

--- home/views.py
+++ home/views.py
@@ -1,25 +1,45 @@
 from django.shortcuts import render
+from django.http.response import HttpResponseRedirect, HttpResponse
 from SPARQLWrapper import SPARQLWrapper, JSON
 import rdflib
+import urllib.parse
 
 
 def homepage(request):
-    # TODO: get genre from ttl static file
+    if request.GET:
+        url = "/movies/?" + urllib.parse.urlencode(request.GET) + "#search-result"
+        return HttpResponseRedirect(url)
+
+    # TODO: get genre from backend function
     ctx = {"genre": ["Action", "Romance", "Comedy"]}
     return render(request, 'homepage.html', ctx)
 
 
-def actor_detail(request):
-    data = {'name': 'Jenna Ortega'}
-    return render(request, 'actor_detail.html', data)
-
-
 def movie_search(request):
-    data = {'movies': [{'title': 'Wednesday'}, {'title': 'Django Unchained'}]}
+    # TODO: get movies and genre from backend function
+    ctx = {'movies': [
+        {'id': 2, 'title': 'Wednesday', 'director': 'Christopher Nolan', 'rating': 4.8, 'genre': 'Thriller'}, 
+        {'id': 1, 'title': 'Django Unchained', 'director': 'Sule Sutrisna', 'rating': 2.4, 'genre': 'Comedy'},
+        {'id': 1, 'title': 'Django Unchained', 'director': 'Sule Sutrisna', 'rating': 2.4, 'genre': 'Comedy'},
+        {'id': 1, 'title': 'Django Unchained', 'director': 'Sule Sutrisna', 'rating': 2.4, 'genre': 'Comedy'},
+        {'id': 1, 'title': 'Django Unchained', 'director': 'Sule Sutrisna', 'rating': 2.4, 'genre': 'Comedy'},
+        {'id': 1, 'title': 'Django Unchained', 'director': 'Sule Sutrisna', 'rating': 2.4, 'genre': 'Comedy'},
+        {'id': 1, 'title': 'Django Unchained', 'director': 'Sule Sutrisna', 'rating': 2.4, 'genre': 'Comedy'},
+        {'id': 1, 'title': 'Django Unchained', 'director': 'Sule Sutrisna', 'rating': 2.4, 'genre': 'Comedy'},
+        {'id': 1, 'title': 'Django Unchained', 'director': 'Sule Sutrisna', 'rating': 2.4, 'genre': 'Comedy'},
+        {'id': 1, 'title': 'Django Unchained', 'director': 'Sule Sutrisna', 'rating': 2.4, 'genre': 'Comedy'},
+        {'id': 1, 'title': 'Django Unchained', 'director': 'Sule Sutrisna', 'rating': 2.4, 'genre': 'Comedy'},
+    ]}
+    ctx['genre'] = ["Action", "Romance", "Comedy"]
 
-    return render(request, 'movie_search.html', data)
+    return render(request, 'movie_search.html', ctx)
 
 
 def movie_detail(request):
     data = {'title': 'Wednesday', 'Actors': [{'name': 'Jenna Ortega'}]}
     return render(request, 'movie_detail.html', data)
+
+
+def actor_detail(request):
+    data = {'name': 'Jenna Ortega'}
+    return render(request, 'actor_detail.html', data)
