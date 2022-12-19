dbb4b2322cb3ffdd0b107aba34eefe87fc757587
RahadianPanji <63046522+RahadianPanji@users.noreply.github.com>
2022-12-20 03:25:40 +0700

    [Frontend] Implement Homepage, Movie Search, and Movie Detail (#1) (#2)
    * create homepage
    
    * fix homepage and implement movie-search
    
    * change var name to id
    
    * implement Movie Detail
    
    * fix data dummy

--- home/views.py
+++ home/views.py
@@ -1,24 +1,74 @@
 from django.shortcuts import render
+from django.http.response import HttpResponseRedirect, HttpResponse
 from SPARQLWrapper import SPARQLWrapper, JSON
 import rdflib
+import urllib.parse
 
 
-def index(request):
-    data = {'genres': ['action', 'romance', 'comedy']}  # stubs, TODO: query from ttl to get list of genre
-    return render(request, 'home.html', data)
+def homepage(request):
+    if request.GET:
+        url = "/movies/?" + urllib.parse.urlencode(request.GET) + "#search-result"
+        return HttpResponseRedirect(url)
+
+    # TODO: get genre from backend function
+    ctx = {"genre": ["Action", "Romance", "Comedy"]}
+    return render(request, 'homepage.html', ctx)
+
+
+def movie_search(request):
+    # TODO: get movies and genre from backend function
+    ctx = {'movies': [
+        {'id': 2, 'title': 'Wednesday', 'director': 'Christopher Nolan', 'rating': 4.8, 'genre': 'Thriller'}, 
+        {'id': 1, 'title': 'Django Unchained', 'director': 'Sule Sutrisna', 'rating': 2.4, 'genre': 'Comedy'},
+        {'id': 1, 'title': 'Django Unchained', 'director': 'Sule Sutrisna', 'rating': 2.4, 'genre': 'Comedy'},
+        {'id': 1, 'title': 'Django Unchained', 'director': 'Sule Sutrisna', 'rating': 2.4, 'genre': 'Comedy'},
+        {'id': 1, 'title': 'Django Unchained', 'director': 'Sule Sutrisna', 'rating': 2.4, 'genre': 'Comedy'},
+        {'id': 1, 'title': 'Django Unchained', 'director': 'Sule Sutrisna', 'rating': 2.4, 'genre': 'Comedy'},
+        {'id': 1, 'title': 'Django Unchained', 'director': 'Sule Sutrisna', 'rating': 2.4, 'genre': 'Comedy'},
+        {'id': 1, 'title': 'Django Unchained', 'director': 'Sule Sutrisna', 'rating': 2.4, 'genre': 'Comedy'},
+        {'id': 1, 'title': 'Django Unchained', 'director': 'Sule Sutrisna', 'rating': 2.4, 'genre': 'Comedy'},
+        {'id': 1, 'title': 'Django Unchained', 'director': 'Sule Sutrisna', 'rating': 2.4, 'genre': 'Comedy'},
+        {'id': 1, 'title': 'Django Unchained', 'director': 'Sule Sutrisna', 'rating': 2.4, 'genre': 'Comedy'},
+    ]}
+    ctx['genre'] = ["Action", "Romance", "Comedy"]
+
+    return render(request, 'movie_search.html', ctx)
+
+
+def movie_detail(request, movie_id):
+    # TODO: get detail from backend function
+    ctx = {
+        'id': 2, 
+        'title': 'Wednesday', 
+        'description': 'Hari Rabu',
+        'director': 'Christopher Nolan', 
+        'rating': 4.8, 
+        'genre': 'Thriller',
+        'actors': [{'id': 1, 'name': 'Sule'}, {'id': 2, 'name': 'Andre'}],
+        'year': 2021,
+        'runtime': 201,
+        'votes': 12,
+        'rank': 2,
+        'revenue': 128
+    }
+    if movie_id == "1":
+        ctx = {
+            'id': 1, 
+            'title': 'wot u doin step brother', 
+            'description': 'Stuck in washing machine',
+            'director': 'Pronbuh', 
+            'rating': 9.9, 
+            'genre': 'Family',
+            'actors': [{'id': 3, 'name': 'Sis'}, {'id': 4, 'name': 'Bro'}],
+            'year': 2077,
+            'runtime': 15,
+            'votes': 51534,
+            'rank': 1,
+            'revenue': 696969
+        }
+    return render(request, 'movie_detail.html', ctx)
 
 
 def actor_detail(request):
     data = {'name': 'Jenna Ortega'}
     return render(request, 'actor_detail.html', data)
-
-
-def movie_search(request):
-    data = {'movies': [{'title': 'Wednesday'}, {'title': 'Django Unchained'}]}
-
-    return render(request, 'movie_search.html', data)
-
-
-def movie_detail(request):
-    data = {'title': 'Wednesday', 'Actors': [{'name': 'Jenna Ortega'}]}
-    return render(request, 'movie_detail.html', data)
--- k-movies/urls.py
+++ k-movies/urls.py
@@ -1,4 +1,4 @@
-"""k-movies URL Configuration
+"""django_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('home.urls'))
-]
+    path('', include('home.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
