747b0d7df4fda043621b0a5a5c573226ab9c595d
Rizky Juniastiar <72258623+rizkyyjun@users.noreply.github.com>
2022-12-23 00:06:24 +0700

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     path('', views.homepage, name='homepage'),
     path('movies/', views.movie_search, name='movie_search'),
     path('movies/<str:movie_id>', views.movie_detail, name='movie_detail'),
-    path('actors/<str:actor_id>', views.actor_detail, name='actor_detail'),
 ]
