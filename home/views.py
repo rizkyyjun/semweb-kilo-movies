714547ab4724d38404fcd5ddfcabcb9d96253a3e
RahadianPanji <63046522+RahadianPanji@users.noreply.github.com>
2022-12-22 23:44:16 +0700

    Implement Movie Search query and adjust frontend (#14)
    * feat: implement movie search based on title, actor, director, and genre
    
    * feat: implement rating based filtering
    
    * refactored filters, now handles empty parameters
    
    * refactored query
    
    * fixed typo
    
    * Adjust homepage and cleanse code (#13)
    
    * create homepage
    
    * fix homepage and implement movie-search
    
    * change var name to id
    
    * implement Movie Detail
    
    * fix data dummy
    
    * fix movie href
    
    * remove year filtering
    
    * cleanse file and code
    
    Co-authored-by: gbhisma <bhismagregorius@gmail.com>
    Co-authored-by: Gregorius Bhisma <67036115+gbhisma@users.noreply.github.com>

--- home/views.py
+++ home/views.py
@@ -11,26 +11,90 @@ def homepage(request):
         return HttpResponseRedirect(url)
 
     # TODO: get genre from backend function
-    ctx = {"genre": ["Action", "Romance", "Comedy"]}
+    ctx = {"genre": genre_query()}
     return render(request, 'homepage.html', ctx)
 
 
+def movie_search_query(title, actor, director, genre, ratingMoreThan, ratingLessThan):
+    title_filter = f"FILTER regex(lcase(?title), '{title.lower()}') ." if title != "" else ""
+    actor_filter = f"FILTER regex(lcase(?actorName), '{actor.lower()}') ." if actor != "" else ""
+    director_filter = f"FILTER regex(lcase(?directorName), '{director.lower()}') ." if director != "" else ""
+    genre_filter = f"FILTER regex(lcase(?genreLabel), '{genre.lower()}') ." if genre != "" else ""
+
+    # yearAfter = yearAfter if yearAfter != "" else 1990
+    # yearBefore = yearBefore if yearBefore != "" else 3000
+    # # year_filter = f"FILTER (?year >= '{yearAfter}'^^xsd:gYear && ?year <= '{yearBefore}'^^xsd:gYear ) ."
+
+    ratingMoreThan = ratingMoreThan if ratingMoreThan != "" else 0.0
+    ratingLessThan = ratingLessThan if ratingLessThan != "" else 10000.0
+    rating_filter = f"FILTER (?rating >= '{ratingMoreThan}'^^xsd:double && ?rating <= '{ratingLessThan}'^^xsd:double )."
+
+    query = """
+        SELECT ?movie ?title ?directorName ?rating (GROUP_CONCAT(distinct(?genreLabel);SEPARATOR=", ") AS ?genres) ?year
+    WHERE {
+      ?movie rdf:type :Movie .
+      OPTIONAL {{?movie :director ?director. 
+                 ?director rdfs:label ?directorName .}}
+      OPTIONAL {{?movie :actors ?actor .
+                 ?actor rdfs:label ?actorName . }}
+      OPTIONAL {{?movie :genre ?genre .
+                 ?genre rdfs:label ?genreLabel .}}
+      OPTIONAL {{?movie :rating ?rating .}}
+      OPTIONAL {{?movie :title ?title .}}
+      OPTIONAL {{?movie :year ?year .}}
+      
+      %s
+      %s
+      %s
+      %s
+      %s
+         
+    } GROUP BY ?movie ?title ?directorName ?desc ?rating ?year ?runtime ?votes ?rank ?revenue
+        """ % (title_filter, actor_filter, director_filter, genre_filter, rating_filter)
+
+    print(query)
+
+    g = rdflib.Graph()
+    g.parse('home/static/movies.ttl')
+    try:
+        res = g.query(query)
+        print("halo")
+        res = process_result(res, "movie_search")
+
+        return res
+    except Exception:
+        return "error"
+
+
+def genre_query():
+    query = """
+    SELECT (GROUP_CONCAT(distinct(?genreLabel);SEPARATOR=", ") AS ?genres)
+    WHERE {
+      ?genre rdf:type :Genre ;
+             rdfs:label ?genreLabel .
+    }
+    """
+    g = rdflib.Graph()
+    g.parse('home/static/movies.ttl')
+    try:
+        res = g.query(query)
+        res = process_result(res, "genre")
+
+        return res
+    except Exception:
+        return "error"
+
+
 def movie_search(request):
-    # TODO: get movies and genre from backend function
-    ctx = {'movies': [
-        {'id': 2, 'title': 'Wednesday', 'director': 'Christopher Nolan', 'rating': 4.8, 'genre': 'Thriller'},
-        {'id': 1, 'title': 'Django Unchained', 'director': 'Sule Sutrisna', 'rating': 2.4, 'genre': 'Comedy'},
-        {'id': 1, 'title': 'Django Unchained', 'director': 'Sule Sutrisna', 'rating': 2.4, 'genre': 'Comedy'},
-        {'id': 1, 'title': 'Django Unchained', 'director': 'Sule Sutrisna', 'rating': 2.4, 'genre': 'Comedy'},
-        {'id': 1, 'title': 'Django Unchained', 'director': 'Sule Sutrisna', 'rating': 2.4, 'genre': 'Comedy'},
-        {'id': 1, 'title': 'Django Unchained', 'director': 'Sule Sutrisna', 'rating': 2.4, 'genre': 'Comedy'},
-        {'id': 1, 'title': 'Django Unchained', 'director': 'Sule Sutrisna', 'rating': 2.4, 'genre': 'Comedy'},
-        {'id': 1, 'title': 'Django Unchained', 'director': 'Sule Sutrisna', 'rating': 2.4, 'genre': 'Comedy'},
-        {'id': 1, 'title': 'Django Unchained', 'director': 'Sule Sutrisna', 'rating': 2.4, 'genre': 'Comedy'},
-        {'id': 1, 'title': 'Django Unchained', 'director': 'Sule Sutrisna', 'rating': 2.4, 'genre': 'Comedy'},
-        {'id': 1, 'title': 'Django Unchained', 'director': 'Sule Sutrisna', 'rating': 2.4, 'genre': 'Comedy'},
-    ]}
-    ctx['genre'] = ["Action", "Romance", "Comedy"]
+    title = request.GET.get('title')
+    actor = request.GET.get('actor')
+    director = request.GET.get('director')
+    genre = request.GET.get('genre')
+    ratingMoreThan = request.GET.get('ratingMoreThan')
+    ratingLessThan = request.GET.get('ratingLessThan')
+
+    ctx = {'movies': movie_search_query(title, actor, director, genre, ratingMoreThan,
+                                        ratingLessThan), 'genre': genre_query()}
 
     return render(request, 'movie_search.html', ctx)
 
@@ -63,36 +127,49 @@ def movie_detail_query(movie_id):
     g.parse('home/static/movies.ttl')
     try:
         res = g.query(query)
-        res = process_result(res)
+        res = process_result(res, "movie_detail")
 
         return res
     except Exception:
         return "error"
 
 
-def process_result(result):
-    res = {}
-
+def process_result(result, query_type):
+    res = []
+    print(len(result))
     for row in result:
-        actors = []
-        for key, value in dict(zip(row.actorsIRI.toPython().split(', '), row.actorNames.toPython().split(', '))).items():
-            actors.append({'id': key, 'name': value})
+        if query_type == "movie_detail":
+            actors = []
+            for key, value in dict(
+                    zip(row.actorsIRI.toPython().split(', '), row.actorNames.toPython().split(', '))).items():
+                actors.append({'id': key, 'name': value})
 
-        temp = {
-            'id': row.movie.toPython(),
-            'title': row.title.toPython(),
-            'description': row.desc.toPython(),
-            'director': row.directorName.toPython(),
-            'rating': row.rating.toPython(),
-            'genre': row.genres.toPython(),
-            'actors': actors,
-            'year': row.year.toPython(),
-            'runtime': row.runtime.toPython(),
-            'votes': row.votes.toPython(),
-            'rank': row.rank.toPython(),
-            'revenue': row.revenue.toPython()
-        }
-        res = temp
+            temp = {
+                'id': row.movie.toPython(),
+                'title': row.title.toPython(),
+                'description': row.desc.toPython(),
+                'director': row.directorName.toPython(),
+                'rating': row.rating.toPython(),
+                'genre': row.genres.toPython(),
+                'actors': actors,
+                'year': row.year.toPython(),
+                'runtime': row.runtime.toPython(),
+                'votes': row.votes.toPython(),
+                'rank': row.rank.toPython(),
+                'revenue': row.revenue.toPython()
+            }
+            return temp
+        elif query_type == "movie_search":
+            temp = {
+                'id': row.movie.toPython(),
+                'title': row.title.toPython(),
+                'director': row.directorName.toPython(),
+                'rating': row.rating.toPython(),
+                'genre': row.genres.toPython(),
+            }
+            res.append(temp)
+        elif query_type == "genre":
+            return row.genres.toPython().split(", ")
 
     return res
 
@@ -100,41 +177,3 @@ def process_result(result):
 def movie_detail(request, movie_id):
     ctx = movie_detail_query(movie_id)
     return render(request, 'movie_detail.html', ctx)
-
-
-def actor_detail(request):
-    data = {'name': 'Jenna Ortega'}
-    return render(request, 'actor_detail.html', data)
-
-def actor_detail_query(actor_name):
-    actor_name = f'dbp:{actor_name}'
-    sparql = SPARQLWrapper('https://dbpedia.org/sparql')
-    query = '''
-    SELECT *
-    WHERE {
-        ?film dbo:starring ?actor .
-        ?actor dbo:abstract ?abstract;
-            dbo:birthDate ?birthDate;
-            dbp:name ?name;
-            dbp:nationality ?nationality.
-        FILTER (lang(?abstract) = "en" && lang(?name) = "en" && lang(?nationality) = "en")
-    }
-    LIMIT 1
-    '''
-
-    sparql.setQuery(query)
-    sparql.setReturnFormat(JSON)
-
-    try:
-        qres = sparql.query().convert()
-        for res in qres['results']['bindings']:
-            print(res)
-            break
-        return qres
-    except Exception:
-        return "SPARQL Error!"
-
-def movie_search(request):
-    data = {'movies': [{'title': 'Wednesday'}, {'title': 'Django Unchained'}]}
-
-    return render(request, 'movie_search.html', data)
